6a781595d6e90918c3745d870550f5c080390eff
Hoang801204 <osasisez@gmail.com>
2025-04-01 05:19:58 +0700

    Your commit message

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello, World!1111");
-=======
-        System.out.println("Hello, World!1122");
->>>>>>> 9a89c6c (Commit uncommitted changes)
+
     }
 }
